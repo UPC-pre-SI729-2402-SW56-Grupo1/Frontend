0131659a79d0f7ae8f8a30ad6549c59e773f917c
cristianipa7 <cristianir007@gmail.com>
2024-09-23 20:09:26 -0500

    feat(app component): add rating component

---
+++ src/app/book-preview/rating/rating.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-rating',
+  standalone: true,
+  imports: [],
+  templateUrl: './rating.component.html',
+  styleUrl: './rating.component.css'
+})
+export class RatingComponent {
+  @Input() rating!: number;
+}
